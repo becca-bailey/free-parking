e1f9472796535a24ff43ca5fac2342ae076076b1
Becca Nelson <beccanelson88@gmail.com>
2016-01-04 14:49:45 -0600

    Add devise authentication

--- free-parking/app/models/user.rb
+++ free-parking/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :owned_games
   has_many :games, through: :owned_games
   has_many :groupings
