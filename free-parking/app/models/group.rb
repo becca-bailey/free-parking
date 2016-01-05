8b0c3fdfc45dc4b2e61e7083d59395df4df87405
Becca Nelson & Jamie Runyan <pair+beccanelson.jreeserunyan@devbootcamp.com>
2016-01-05 14:38:29 -0600

    Change root folder

--- free-parking/app/models/group.rb
+++
@@ -1,5 +0,0 @@
-class Group < ActiveRecord::Base
-  has_many :groupings
-  has_many :members, through: :groupings, source: :user
-  has_many :games, through: :members
-end
--- free-parking/app/models/owned_game.rb
+++
@@ -1,4 +0,0 @@
-class OwnedGame < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :game
-end
