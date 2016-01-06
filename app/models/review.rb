fa4fede754318c33740269712e02d2b5521681c4
Josh Abrams & Colin Razevich <pair+frizzbolt.razevich@devbootcamp.com>
2016-01-06 09:35:42 -0600

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
   belongs_to :game
+
+  validates_numericality_of :fun_rating, :only_integer => true, :message => "Fun rating must be a whole number."
+  validates_numericality_of :difficulty_rating, :only_integer => true, :message => "Difficulty rating must be a whole number."
+
 end
