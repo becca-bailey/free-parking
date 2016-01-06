c8bfffe886b77454605491f3e441f62d362dd461
Becca Nelson & Jamie Runyan <pair+beccanelson.jreeserunyan@devbootcamp.com>
2016-01-06 10:34:18 -0600

    secure invite routes

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -41,7 +41,9 @@ class InvitesController < ApplicationController
   def join
     invite = Invite.find(params[:id])
     game_group = Group.find(params[:group_id])
-    current_user.groups << game_group
+    unless member?
+      current_user.groups << game_group
+    end
     session.delete(:token)
     redirect_to group_path(game_group)
   end
@@ -52,9 +54,13 @@ class InvitesController < ApplicationController
     params.require(:invite).permit(:email, :token)
   end
 
-  def check_membership
+  def member?
     @group = Group.find(params[:group_id])
-    unless @group.members.include?(current_user)
+    @group.members.include?(current_user)
+  end
+
+  def check_membership
+    unless member?
       flash[:notice] = "You must be a member to invite people to join this group.."
       redirect_to login_path
     end
--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,2 +1,5 @@
 module InvitesHelper
+  def check_membership(group)
+    group.members.include?(current_user)
+  end
 end
